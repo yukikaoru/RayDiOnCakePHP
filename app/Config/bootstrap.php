1cf516054be4bb3f71c486419912c496b3deca67
YUKI Kaoru <yuki@yagni.jp>
2013-07-17 12:12:52 +0900

    autoloaderの設定を追加

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -23,6 +23,13 @@
  * @since         CakePHP(tm) v 0.10.8.2117
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+/**
+ * Register autoloader for composer
+ */
+$loader = require __DIR__ . '/../../vendors/autoload.php';
+AnnotationRegistry::registerLoader([$loader, "loadClass"]);
 
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File'));
@@ -89,8 +96,8 @@ Cache::config('default', array('engine' => 'File'));
  * ));
  */
 Configure::write('Dispatcher.filters', array(
-	'AssetDispatcher',
-	'CacheDispatcher'
+    'AssetDispatcher',
+    'CacheDispatcher'
 ));
 
 /**
@@ -98,12 +105,12 @@ Configure::write('Dispatcher.filters', array(
  */
 App::uses('CakeLog', 'Log');
 CakeLog::config('debug', array(
-	'engine' => 'FileLog',
-	'types' => array('notice', 'info', 'debug'),
-	'file' => 'debug',
+    'engine' => 'FileLog',
+    'types' => array('notice', 'info', 'debug'),
+    'file' => 'debug',
 ));
 CakeLog::config('error', array(
-	'engine' => 'FileLog',
-	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
-	'file' => 'error',
+    'engine' => 'FileLog',
+    'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
+    'file' => 'error',
 ));
